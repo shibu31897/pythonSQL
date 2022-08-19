5a44a8c9dc2edbb47de95b0de93d36d26ad25552
Harshit Gupta <harshitg31897@outlook.com>
2022-08-20 00:55:20 +0530

    SQLite Implemented

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import sqlite3
 app = Flask(__name__)
 
 all_books = []
-
+db = sqlite3.connect("books-collection.db")
+cursor = db.cursor()
+cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 
 @app.route('/')
 def home():
@@ -18,6 +20,8 @@ def add():
             "author" : request.form['author'],
             "rating" : request.form['rating']
         }
+        cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+        db.commit()
         all_books.append(new_book)
         return redirect(url_for('home'))
     print(all_books)
